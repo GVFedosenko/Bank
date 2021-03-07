f13698fa2a930bf2977163121d6e6414a32327b8
Kenton <fedosenkogeorgij44@gmail.com>
2021-03-07 10:47:51 +0300

    Добавлена возможность добавлять клиента и счет, исправлена дата

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -28,7 +28,7 @@ namespace Bank
                 {
                     case "1":
                         string sql =
-                            $"SELECT table_Bank.id, table_Bank.firstName, table_Bank.lastName, table_Bank.patronymic, table_credit.sumCredit, table_contribution.sumContribution from table_Bank join table_credit join table_contribution on table_Bank.id = table_credit.id and table_Bank.id = table_contribution.id;";
+                            $"SELECT table_Bank.id, table_Bank.firstName, table_Bank.lastName, table_Bank.patronymic, table_Bank.birthday from table_Bank;";
                         MySqlCommand command = new MySqlCommand
                         {
                             Connection = connection, CommandText = sql
@@ -36,17 +36,88 @@ namespace Bank
                         MySqlDataReader result = command.ExecuteReader();
                         while (result.Read())
                         {
-                            string tempName0 = result.GetString("id");
+                            string tempName0 = result.GetString("numberClient");
                             string tempName1 = result.GetString("firstName");
                             string tempName2 = result.GetString("lastName");
                             string tempName3 = result.GetString("patronymic");
-                            string tempName4 = result.GetString("sumCredit");
-                            string tempName5 = result.GetString("sumContribution");
-                            Console.WriteLine($"{tempName0} - {tempName1} - {tempName2} - {tempName3} - {tempName4} - {tempName5}");
+                            string tempname6 = result.GetString("birthday");
+                            Console.WriteLine(
+                                $"{tempName0} - {tempName1} - {tempName2} - {tempName3} - {tempname6}");
+                        }
+                        connection.Close();
+                        break;
+                    case "2":
+                        connection.Close();
+                        Console.Write("Фамилия: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Имя: ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Отчество: ");
+                        string patronymic = Console.ReadLine();
+                        Console.Write("Дата рождания: ");
+                        string birthday = Console.ReadLine();
+                        connection.Open();
+                        sql = $"insert table_Bank(firstName, lastName, patronymic, birthday) value('{firstName}', '{lastName}', '{patronymic}', '{birthday}');";
+                        MySqlCommand command1 = new MySqlCommand
+                        {
+                            Connection = connection, CommandText = sql
+                        };
+                        int res = command1.ExecuteNonQuery();
+                        if (res == 0)
+                        {
+                            Console.WriteLine("Данные не добавились((");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Данные добавились!");
+                        }
+                        
+                        connection.Close();
+                        break;
+                    case "3":
+                        connection.Close();
+                        string sql2 =
+                            $"SELECT table_Bank.id, table_Bank.firstName, table_Bank.lastName, table_Bank.patronymic, table_Bank.birthday from table_Bank;";
+                        MySqlCommand command2 = new MySqlCommand
+                        {
+                            Connection = connection, CommandText = sql2
+                        };
+                        var result1 = command2.ExecuteReader();
+                        while (result1.Read())
+                        {
+                            string tempName0 = result1.GetString("numberClient");
+                            string tempName1 = result1.GetString("firstName");
+                            string tempName2 = result1.GetString("lastName");
+                            string tempName3 = result1.GetString("patronymic");
+                            string tempname6 = result1.GetString("birthday");
+                            Console.WriteLine(
+                                $"{tempName0} - {tempName1} - {tempName2} - {tempName3} - {tempname6}");
+                        }
+                        Console.WriteLine("Введите номер клента, для которого желаете добавить счет: ");
+                        int numberClient = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите сумму счета: ");
+                        int sumContribution = Convert.ToInt32(Console.ReadLine());
+                        string sql3 =
+                            $"INSERT table_contribution(id, sumContribution) value('{numberClient}', '{sumContribution}');";
+                        MySqlCommand command3 = new MySqlCommand
+                        {
+                            Connection = connection, CommandText = sql3
+                        };
+                        int res1 = command3.ExecuteNonQuery();
+                        if (res1 == 0)
+                        {
+                            Console.WriteLine("Данные не добавились((");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Данные добавились!");
                         }
                         connection.Close();
                         break;
                 }
+                
             }
             else
             {
